5bb753bfa17a7606eae21df390e5fb110ea614f3
최다인 <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-01 13:27:13 +0900

    Update 알파벳 찾기.java

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -8,14 +8,15 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
+		StringBuffer sb = new StringBuffer(); // String은 변경 불가능한 문자열 vs StringBuffer 변경가능
 
 		String s = br.readLine();
 
-		int[] arr = new int[26];
-		Arrays.fill(arr, -1);
+		int[] arr = new int[26]; // 알파벳 길이만큼 배열
+		Arrays.fill(arr, -1); // 배열 26칸을 -1로 초기화
 
 		for (int i = 0; i < s.length(); i++) {
+			// 배열 값이 -1이 아닐 때 이미 알파벳이 나왔기 때문에 continue
 			if (arr[s.charAt(i) - 'a'] != -1) {
 				continue;
 			} else {
