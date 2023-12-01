297ffaed73232827456bcd1cb6d48e4194693370
최다인 <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-01 16:59:04 +0900

    Update 단어의 개수.java

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -4,6 +4,8 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) {
+
+		// 방법 1
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
 		sc.close();
@@ -11,5 +13,16 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(str, " ");
 		
 		System.out.println(st.countTokens());
+
+		/* 방법 2
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		System.out.print(st.countTokens());
+
+		*/
+
+
 	}
 }
