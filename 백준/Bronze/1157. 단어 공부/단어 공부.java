b66429764d760993df14fea5de01c83d5c907e08
최다인 <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-04 12:52:49 +0900

    Update 단어 공부.java

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -14,15 +14,15 @@ public class Main {
 		String st = br.readLine();
 		for (int i = 0; i < st.length(); i++) {
 			if ('A' <= st.charAt(i) && st.charAt(i) <= 'Z') {
-				arr[st.charAt(i) - 'A']++;
+				arr[st.charAt(i) - 'A']++; // 대문자 A-Z까지 나온 횟수 증가
 			} else
-				arr[st.charAt(i) - 'a']++;
+				arr[st.charAt(i) - 'a']++; // 소문자 a-z까지 나온 횟수 증가
 		}
 
-		for (int i = 0; i < 26; i++) {
+		for (int i = 0; i < 26; i++) { // arr 전부 검색
 			if (arr[i] > max) {
-				max = arr[i];
-				ch = (char) (i + 65);
+				max = arr[i]; // 제일 많이 나온 arr[i]<문자>를 구함
+				ch = (char) (i + 65); // arr[i]<문자>에 +65로 대문자로 만들어주기
 			} else if (arr[i] == max)
 				ch = '?';
 		}
