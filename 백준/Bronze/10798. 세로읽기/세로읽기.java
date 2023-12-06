deb2b1852e24a7970a4a6abd0959202f65aa1304
최다인 <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-06 10:38:13 +0900

    [Bronze I] Title: 세로읽기, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String[][] arr = new String[5][15];
+
+		for (int i = 0; i < 5; i++) {
+			String s = sc.next();
+			while (s.length() != 15) {
+				s += "#";
+			}
+			arr[i] = s.split("");
+		}
+
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (!arr[j][i].equals("#"))
+					System.out.print(arr[j][i]);
+			}
+		}
+	}
+
+}
