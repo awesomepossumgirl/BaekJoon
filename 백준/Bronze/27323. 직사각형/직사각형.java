9374ae82b02d88e2cb928699ebba1288b7fd5bd0
AwesomeDain <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-11 14:58:27 +0900

    [Bronze V] Title: 직사각형, Time: 128 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int result = A*B;
+		
+		bw.write(result+"");
+		bw.close();
+	}
+
+}
