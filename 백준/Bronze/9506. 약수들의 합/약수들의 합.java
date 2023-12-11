b9377bfa65e3d1abc09f492b11e048744effbc83
AwesomeDain <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-11 13:37:34 +0900

    [Bronze I] Title: 약수들의 합, Time: 156 ms, Memory: 15100 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -3,30 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while(true){
-            StringBuilder sb = new StringBuilder();
-            int n = Integer.parseInt(br.readLine());
-            if(n == -1) break;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            sb.append(n + " = 1");
-            int sum = 1;
-            for(int i = 2; i < n; i++){
-                if(n%i == 0){
-                    sb.append(" + " + i);
-                    sum+=i;
-                }
-            }
+		while (true) {
+			StringBuilder sb = new StringBuilder();
+			int n = Integer.parseInt(br.readLine());
+			if (n == -1) break;
 
-            if(n == sum){
-                System.out.println(sb);
-            }
-            else{
-                System.out.println(n + " is NOT perfect.");
-            }
-        }
-        br.close();
-    }
+			sb.append(n + " = 1");
+			int sum = 1;
+			for (int i = 2; i < n; i++) {
+				if (n % i == 0) {
+					sb.append(" + " + i);
+					sum += i;
+				}
+			}
+			if (n == sum) {
+				System.out.println(sb);
+			} else {
+				System.out.println(n + " is NOT perfect.");
+			}
+		}
+		br.close();
+	}
+
 }
