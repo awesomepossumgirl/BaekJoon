bb187c7cae3778a2104736d1498443e8e054276d
AwesomeDain <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-13 16:00:34 +0900

    Update 대표값2.java

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -14,5 +14,20 @@ public class Main {
 		Arrays.sort(arr);
 		System.out.println(sum/5);
 		System.out.println(arr[2]);
+
+		/*
+	       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int sum = 0;  //수들의 합
+	        int mid = 0;  //중앙값
+	        int[] arr = new int[5];
+	        for (int i = 0; i < 5; i++) {
+	            arr[i] = Integer.parseInt(br.readLine());
+	            sum += arr[i];
+	        }
+	        Arrays.sort(arr);  //배열 오름차순으로 정렬
+	        int avg = sum / 5;  //평균 구하기
+	        mid = arr[2];
+	        System.out.print(avg + "\n" + mid);
+	  	*/
 	}
 }
