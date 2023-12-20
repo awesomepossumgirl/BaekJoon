ab3ea68ee5926d11041e30c5a88e6ee58a52fb28
AwesomeDain <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-20 17:27:31 +0900

    Update 영단어 암기는 괴로워.py

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -15,3 +15,11 @@ word_list = sorted(word_list.items(), key=lambda x:(-x[1], -len(x[0]), x[0]))
 
 for word,count in word_list:
     print(word)
+
+# x[0] = 단어, x[1] = 단어 빈도수
+# -x[1] = 자주 나오는 단어 앞에 배치
+# -len(x[0]) = 단어 길이 길수록 앞에 배치
+# x[0] = 단어 사전 순 정렬
+
+"""sorted 사용 시 다중 조건으로 정렬 가능하니 알아두자"""
+
