c8c2027509781f5159270828fa66166aca844897
AwesomeDain <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-20 13:50:57 +0900

    Update 카드2.py

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -8,3 +8,16 @@ while(len(deque)>1):
     temp = deque.popleft()
     deque.append(temp)
 print(deque[0])
+"""
+#시간초과난 코드
+
+N = int(input())
+L = list(range(1,N+1))
+
+while len(L)>1:
+    L.pop(0)
+    temp = L.pop(0)
+    L.append(temp)
+
+print(L.pop(0))
+"""
