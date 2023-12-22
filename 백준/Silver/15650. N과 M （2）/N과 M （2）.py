fa174f8bc88356e3cf1c1dc6ddf512013ed4c9e6
AwesomeDain <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-22 11:00:49 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,9 +1,13 @@
-from itertools import combinations
-
 N, M = map(int, input().split())
+li = []
 
-numList = [i for i in range(1, N+1)]
-# [1, 2, 3, 4]
-
-for seq in combinations(numList, M):
-    print(*seq)
+def dfs(start):
+    if len(li)==M:
+        print(' '.join(map(str,li)))
+        return
+    for i in range(start, N+1):
+        if i not in li:
+            li.append(i)
+            dfs(i+1)
+            li.pop()
+dfs(1)
