3a31d0288bdaf48c4a5804d92d51fb94b18e1ac7
AwesomeDain <139747705+awesomepossumgirl@users.noreply.github.com>
2023-12-22 11:01:16 +0900

    Update N과 M （2）.py

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -11,3 +11,16 @@ def dfs(start):
             dfs(i+1)
             li.pop()
 dfs(1)
+
+"""
+#조합을 이용한 풀이
+from itertools import combinations
+
+N, M = map(int, input().split())
+
+numList = [i for i in range(1, N+1)]
+# [1, 2, 3, 4]
+
+for seq in combinations(numList, M):
+    print(*seq)
+"""
